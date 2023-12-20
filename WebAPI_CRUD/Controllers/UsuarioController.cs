8e7a3bc71e28465375a5b9d15d68059856bab5c6
MicaBazan <micabazan5@gmail.com>
2023-12-20 12:55:45 -0300

    Creacion de la WebAPI

---
+++ WebAPI_CRUD/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI_CRUD.Data;
+using WebAPI_CRUD.Models;
+
+namespace WebAPI_CRUD.Controllers
+{
+    // Controller: INterfaz que vamos a estar llamando desde nuestra URL
+    public class UsuarioController
+    {
+        // GET api/<controller>
+        public List<Usuario> Get()
+        {
+            return UsuarioData.Listar();
+        }
+
+        // GET api/<controller>/5
+        public Usuario Get(int id)
+        {
+            return UsuarioData.Obtener(id);
+        }
+
+        // POST api/<controller>
+        public bool Post([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.Registrar(oUsuario);
+        }
+
+        // PUT api/<controller>/5
+        public bool Put([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.Modificar(oUsuario);
+        }
+
+        // DELETE api/<controller>/5
+        public bool Delete(int id)
+        {
+            return UsuarioData.Eliminar(id);
+        }
+    }
+}
---
+++ WebAPI_CRUD/Data/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_CRUD.Data
+{
+    public class Conexion
+    {
+        public static string rutaConexion = "Data Source=MICA/SQLEXPRESS01;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+    }
+}
